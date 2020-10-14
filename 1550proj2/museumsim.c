575755757347a1af2088fda302831a89b05dec63
Keegan Fouse <kdf11@pitt.edu>
2020-10-13 23:47:11 -0400

    updating these boys

---
+++ 1550proj2/museumsim.c
@@ -0,0 +1,11 @@
+/*
+  compile: gcc -g -m32 -o museumsim -I /u/OSLab/kdf11/linux-2.6.23.1/include/ museumsim.c
+*/
+
+#include <linux/unistd.h>
+#include <stdio.h>
+
+int main(){
+  //main routine
+
+}
